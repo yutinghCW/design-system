14d02d4dd957d5a50889031c254e66f9287012bc
yutingh <yutingh@cw.com.tw>
2022-02-25 16:37:11 +0800

    tab js

--- assets/js/cw.js
+++ assets/js/cw.js
@@ -1,12 +1,38 @@
 // Tab Li 切換
-const tabNav = document.querySelectorAll('.tab__nav > ul > li');
-tabNav.forEach((element, index) => {
+const tab_navs = document.querySelectorAll('.tab__nav > ul > li');
+let tab_nav = {
+  li: null,
+  current: null,
+  array: []
+};
+let tab_content = {
+  index: null,
+};
+tab_navs.forEach((element) => {
   element.addEventListener("click", function (e) {
-    let tabNavLi = e.target.parentNode.children;
-    for (let i = 0; i < tabNavLi.length; i++) {
-      const item = tabNavLi[i];
-      item.classList.remove('active');
+    tab_nav.array = [];
+    if ( e.target.tagName.toLowerCase() === 'li' ) {
+      tab_nav.li = e.target.parentNode.children;
+      tab_nav.current = e.target;
+      // tab_content.index = 
+    } else if ( e.target.tagName.toLowerCase() === 'span' ) {
+      tab_nav.li = e.target.parentNode.parentNode.children;
+      tab_nav.current = e.target.parentNode;
     }
-    e.target.classList.add('active');
+    for (let i = 0; i < tab_nav.li.length; i++) {
+      const item = tab_nav.li[i];
+      item.classList.remove('active');
+      for (let j = 0; j < item.parentNode.parentNode.parentNode.children[2].children.length; j++) {
+        item.parentNode.parentNode.parentNode.children[2].children[j].classList.remove('active');
+      }
+    }
+    tab_nav.current.classList.add('active');
+
+    for (let k = 0; k < tab_nav.li.length; k++) {
+      if ( tab_nav.li[k].classList[0] === 'active' ) {
+        tab_content.index = k;
+      }
+    }
+    tab_nav.current.parentNode.parentNode.parentNode.children[2].children[tab_content.index].classList.add('active');
   });
 });
