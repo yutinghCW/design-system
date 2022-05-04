4b1ceec84fa41b548dbd08287f2c11a1fc5e220a
yutingh <yutingh@cw.com.tw>
2022-05-04 16:57:32 +0800

    textfield, tab

---
+++ assets/js/textfield.js
@@ -0,0 +1,22 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.min.js';
+const app = createApp({
+    data() {
+        return {
+            tab: true,
+            value: '',
+            option: {
+                counter: false,
+                icon: false,
+            },
+            state: 'none',
+        }
+    },
+    mounted() {
+        hljs.initHighlightingOnLoad();
+    },
+    methods: {
+        login() {
+        },
+    }
+})
+app.mount('#app')
